91213b705ec17e8cc3be678c14c001e74b43a12f
ShaxobiddinMukhiddinov <87309222+ShaxobiddinMukhiddinov@users.noreply.github.com>
2021-08-03 15:07:24 +0500

    Update 2021_08_03_070150_create_students_table.php

--- database/migrations/2021_08_03_070150_create_students_table.php
+++ database/migrations/2021_08_03_070150_create_students_table.php
@@ -28,6 +28,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('students');55555555659874651
     }
 }
